34ca30cbe9fdd5b5aaf45cfeaa07da8b6744a07b
Shuoliu Yang <visysl@outlook.com>
2024-05-11 16:43:11 +0800

    add preview info feature

--- difftool.sh
+++ difftool.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-git diff | fzf \
+git diff ImageResult | fzf \
 		--prompt '(ctrl-a to accept change)> ' \
-		--bind 'enter:execute-silent(mkdir /tmp/diffimgtmp; git show HEAD:{} > /tmp/diffimgtmp/tmp.png;compare /tmp/diffimgtmp/tmp.png {} png:- | montage -geometry +5+4 /tmp/diffimgtmp/tmp.png - {} png:- | display -title "res" -;rm -rf /tmp/diffimgtmp/)' \
-		--preview 'exiftool {} | bat' \
+		--bind 'enter:execute-silent(mkdir /tmp/diffimgtmp; git show HEAD:{} > /tmp/diffimgtmp/tmp.png;compare /tmp/diffimgtmp/tmp.png {} png:- | montage -geometry +4+4 /tmp/diffimgtmp/tmp.png - {} png:- | display -title "res" -;rm -rf /tmp/diffimgtmp/)' \
+		--preview 'mkdir /tmp/diffimgtmp;git show HEAD:{} > /tmp/diffimgtmp/tmp.png; (echo "====New===="; exiftool {}; echo "====Old====";  exiftool /tmp/diffimgtmp/tmp.png)| bat; rm -rf /tmp/diffimgtmp/' \
 		--bind 'ctrl-a:execute-silent(git add {})' \
